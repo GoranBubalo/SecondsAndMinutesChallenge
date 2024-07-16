d02faa70fd463906a3c7861a745bf811ebecb542
Goran Bubalo <gobu@rao.local>
2024-07-16 08:33:18 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(3945)); // first test case
-        System.out.println(getDurationString(65,45)); // seconds test case
+        System.out.println(getDurationString(-3945)); // first test case
+        System.out.println(getDurationString(-65,45)); // seconds test case
+        System.out.println(getDurationString(65,145));
+        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(3945));
 
     }
 
     public static String getDurationString(int seconds){
 
-        int minutes = seconds / 60;
-        int hours = minutes / 60;
+        if (seconds < 0){
+            return  "Invalid data for seconds (" + seconds + "), must be a positive number";
+        }
 
-        int remainingMinutes = minutes % 60;
-        int remainingSeconds = seconds % 60;
-
-        return  hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+        return getDurationString(seconds / 60, seconds % 60);
     }
 
     public static String getDurationString(int minutes, int seconds){
 
+        if (minutes < 0){
+            return  "Invalid data for minutes (" + minutes + "), must be a positive number";
+        }
+
+        if (seconds < 0 || seconds > 59){
+            return "Invalid data input for seconds (" + seconds +") must be a number between 0 and 59";
+        }
 
         int  hours = minutes / 60;
         int remainingMinutes = minutes % 60;
-        int remainingSeconds = seconds % 60;
 
-        return  hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+        return  hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
 }
